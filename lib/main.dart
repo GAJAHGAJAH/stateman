a8465f385f7f2ff5b53e9483e739fa78232a3711
GAJAHGAJAH <1123150165@global.ac.id>
2026-02-26 14:07:12 +0700

    mengubah main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
-import 'package:provider/provider.dart';
-import 'models/counter_model.dart';
+import 'package:flutter/material.dart';
+import 'views/home_page.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => CounterModel(),
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
 }
